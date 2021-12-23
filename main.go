c9f3b03f13cb0eac8a74fd9f635eb86278d5b360
Nicholas Ledan <Nickledan5@gmail.com>
2021-12-23 16:03:02 -0500

    init

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
