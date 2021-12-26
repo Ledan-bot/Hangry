175a9e8450be31cbf6b4ff1e15b56af6fb9ba4c6
Nicholas Ledan <Nickledan5@gmail.com>
2021-12-26 18:06:42 -0500

    Restyled Menu card

--- src/components/menus/menuCard.jsx
+++ src/components/menus/menuCard.jsx
@@ -3,19 +3,22 @@ import Axios from 'axios'
 import './menuCard.css'
 
 export default function MenuCard({menu}) {
-  const {chef, entrees, name, miscellaneous} = menu
+  const {chef, entrees, name, miscellaneous, image} = menu
 
   return (
     <section className="menu-card-container">
-      <p>{chef.description}</p>
+      <div className="card-top">
+        <img src={image} alt="food" className="menu-image"></img>
+        <p className="chef-description">{chef.description}</p>
+      </div>
       <br />
       <div className="card-bottom">
         <div>
-          <h2>{chef.name}'s {name}</h2>
+          <h2 className="card-title">{chef.name}'s {name}</h2>
           <h4>Stars: {miscellaneous.stars}</h4>
           <p>Average Price per Person: ${miscellaneous.average_price}</p>
         </div>
-        <img src={chef.image} className="chef-image"></img>
+        <img src={chef.image} className="chef-image" alt="chef image"></img>
       </div>
     </section>
   )
