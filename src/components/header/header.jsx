bc6e7c1488fd00045fea0d87d94eac0bd4705200
Nicholas Ledan <Nickledan5@gmail.com>
2021-12-28 11:59:43 -0500

    Styled Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import {IconContext} from 'react-icons'
-import {MdOutlineFoodBank} from 'react-icons/md'
+import { IconContext } from 'react-icons'
+import { MdOutlineFoodBank } from 'react-icons/md'
 import './Header.css'
 
 export default function Header() {
   return (
     <header className="hangry-header">
-      <IconContext.Provider value={{color: "red", size: '40%',}}>
-        <MdOutlineFoodBank />
-      </IconContext.Provider>
-      <h1 className="header-title">Hangry</h1>
+      <div className="title-container">
+        <IconContext.Provider value={{ className: "header-react-icon" }}>
+          <MdOutlineFoodBank />
+        </IconContext.Provider>
+        <h1 className="header-title">Hangry</h1>
+      </div>
+      <a href="https://staging.tryhungry.com/login" className="login-link">LOGIN/SIGNUP</a>
     </header>
   )
 }
