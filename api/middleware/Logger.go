94566887bc5f022bd8a54e5c00a2d7ecd0b7cba2
Nicholas Ledan <Nickledan5@gmail.com>
2022-01-03 23:39:05 -0500

    Added Log struct

--- api/middleware/Logger.go
+++ api/middleware/Logger.go
@@ -8,12 +8,11 @@ import (
 )
 
 func Logger(param gin.LogFormatterParams) string {
-	return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+	return fmt.Sprintf("%s - [%s] \"%s %s %d %s \"%s\" %s\"\n",
 		param.ClientIP,
 		param.TimeStamp.Format(time.RFC1123),
 		param.Method,
 		param.Path,
-		param.Request.Proto,
 		param.StatusCode,
 		param.Latency,
 		param.Request.UserAgent(),
